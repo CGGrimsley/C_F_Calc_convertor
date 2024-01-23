131c0f8873d9585dcbcfa1ad075714bd02f2cffc
CGGrimsley <144144388+CGGrimsley@users.noreply.github.com>
2024-01-23 10:20:03 -0500

    Added Kelvin
    -added Kelvin calculations

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ def celsius_to_fahrenheit(celsius):
     return celsius * 9 / 5 + 32
 
 
+def celsius_to_kelvin(celsius):
+    return celsius + 273.15
+
+
+def fahrenheit_to_kelvin(fahrenheit):
+    return ((fahrenheit - 32) * 5 / 9) + 273.15
+
+
+def kelvin_to_celsius(kelvin):
+    return (kelvin - 273.15) * 9 / 5 + 32
+
+
+def kelvin_to_fahrenheit(kelvin):
+    return kelvin + 273.15
+
+
 def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5 / 9
 
@@ -17,19 +33,30 @@ def convert_temperature():
         result_label.config(text=f"{fahrenheit_to_celsius(temperature):.2f} °C")
     elif choice == 2:
         result_label.config(text=f"{celsius_to_fahrenheit(temperature):.2f} °F")
+    elif choice == 3:
+        result_label.config(text=f"{fahrenheit_to_kelvin(temperature):.2f} °K")
+    elif choice == 4:
+        result_label.config(text=f"{celsius_to_kelvin(temperature):.2f} °K")
+    elif choice == 5:
+        result_label.config(text=f"{kelvin_to_celsius(temperature):.2f} °C")
+    elif choice == 6:
+        result_label.config(text=f"{kelvin_to_fahrenheit(temperature):.2f} °F")
 
 
 # Create the main window
 window = tk.Tk()
 window.title("Temperature Converter")
 
-# Create a variable to store the choice (Fahrenheit = 1, Celsius = 2)
 choice_var = tk.IntVar()
-choice_var.set(1)  # Default choice is Fahrenheit
+choice_var.set(1)
 
-# Creates buttons for selecting Celsius or Fahrenheit
-celsius_radio = tk.Radiobutton(window, text="Celsius", variable=choice_var, value=2)
-fahrenheit_radio = tk.Radiobutton(window, text="Fahrenheit", variable=choice_var, value=1)
+# Creates buttons for selecting Celsius or Fahrenheit or Kelvin
+C_to_F_radio = tk.Radiobutton(window, text="°C to °F", variable=choice_var, value=2)
+F_to_C_radio = tk.Radiobutton(window, text="°F to °C", variable=choice_var, value=1)
+F_to_K_radio = tk.Radiobutton(window, text="°F to °K", variable=choice_var, value=3)
+C_to_K_radio = tk.Radiobutton(window, text="°C to °K", variable=choice_var, value=4)
+K_to_C_radio = tk.Radiobutton(window, text="°K to °C", variable=choice_var, value=5)
+K_to_F_radio = tk.Radiobutton(window, text="°K to °F", variable=choice_var, value=6)
 
 # Creates a field for temperature input
 entry_label = tk.Label(window, text="Enter Temperature:")
@@ -42,8 +69,12 @@ convert_button = tk.Button(window, text="Convert", command=convert_temperature)
 result_label = tk.Label(window, text="", font=("Helvetica", 14))
 
 # Put the widgets in the window
-celsius_radio.pack()
-fahrenheit_radio.pack()
+C_to_F_radio.pack()
+F_to_C_radio.pack()
+F_to_K_radio.pack()
+C_to_K_radio.pack()
+K_to_C_radio.pack()
+K_to_F_radio.pack()
 entry_label.pack()
 entry.pack()
 convert_button.pack()
